cdcdd329f07818f167105c5f633283b2fb1e4eea
lokolilo <lokolilo574@gmail.com>
2023-10-17 13:51:18 +0200

    feat: added User Class

---
+++ src/main/java/com/example/springtestapp/model/User.java
@@ -0,0 +1,73 @@
+package com.example.springtestapp.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+//@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String role;
+    private Date birthdate;
+
+
+
+    //Getters & Setters, toString
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", role='" + role + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
+}
